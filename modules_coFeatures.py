2a86845354d984eeacc0660fa45bf1d6a861824f
ESPhoenix <e.s.phoenix@hotmail.co.uk>
2023-11-07 15:45:16 +0000

    modular fucntions

--- modules_coFeatures.py
+++ modules_coFeatures.py
@@ -81,222 +81,171 @@ def get_key_atom_coords(pdbDf,keyAtomsDict,cofactorName):
         keyAtomCoords.loc[atomId] = [xCoord,yCoord,zCoord]
     return keyAtomCoords
 ########################################################################################
-
-def get_orb_center(orbAtomsDict,cofactorName, pdbDf):
-    # get orbAboms that correspond to cofactor present in this pdbfile
+def gen_orb_region(orbAtomsDict, cofactorName, pdbDf, orbValue):
+    ## FIND ORB ATOMS IN PDB DATAFRAME ##
     orbAtoms = orbAtomsDict.get(cofactorName,[])
-
     cofactorRows = pdbDf[pdbDf["RES_NAME"] == cofactorName]
     orbRows = cofactorRows[cofactorRows["ATOM_NAME"].isin(orbAtoms)]
-    
+    # GET ORB CENTER AS AVERAGE POSITION OF ORB ATOMS ##
     orbCenter=[]
     orbCenter.append(orbRows["X"].mean())
     orbCenter.append(orbRows["Y"].mean())
     orbCenter.append(orbRows["Z"].mean())
 
-    return orbCenter
+    ## REMOVE COFACTOR FROM PDB DATAFRAME ##
+    noCofactorDf = pdbDf[pdbDf["RES_NAME"] != cofactorName]
+    
+    ## CALCULATE DISTANCES BETWEEN PROTEIN ATOMS AND 
+    noCofactorDf["orb_DISTANCES"] = np.linalg.norm(noCofactorDf[["x", "y", "z"]].values - np.array(orbCenter), axis=1)
+    ## MAKE ORB DATAFRAME OUT OF ATOMS THAT ARE WITHIN ORBVALUE OF ORB CENTER
+    orbDf = pdbDf[pdbDf["orb_DISTANCES"] <= orbValue].copy()
+
+    return orbDf
 ########################################################################################
-
-def get_cloud_atom_coords(cloudAtomsDict,cofactorName, pdbDf):
+def gen_cloud_region(cloudAtomsDict, cofactorName, pdbDf, cloudValue):
+    ## FIND CLOUD ATOMS IN PDB DATAFRAME ##
     cloudAtoms = cloudAtomsDict.get(cofactorName,[])
-
     cofactorRows = pdbDf[pdbDf["RES_NAME"] == cofactorName]
     cloudRows = cofactorRows[cofactorRows["ATOM_NAME"].isin(cloudAtoms)]
-    
+
+    ## CREATE DATAFRAME CONTAINING COORDINATES OF CLOUD ATOMS ##
     cloudCoordsDf=cloudRows[["ATOM_NAME","X","Y","Z"]]
     cloudCoordsDf.set_index("ATOM_NAME",inplace=True)
 
-    return cloudCoordsDf
-    
+    ## REMOVE COFACTOR FROM PDB DATAFRAME ##
+    noCofactorDf = pdbDf[pdbDf["RES_NAME"] != cofactorName]
+
+    ## LOOP THROUGH CLOUD ATOMS ##
+    for i, (x,y,z) in cloudCoordsDf.iterrows():
+        ## CALCULATE DISTANCE BETWEEN CLOUD ATOMS AND PROTEIN ATOMS ##
+        columnName=f'cloud_dist_atom_{i}'
+        noCofactorDf[columnName] = np.linalg.norm(noCofactorDf[["x", "y", "z"]].values - np.array(x,y,z), axis=1)
+
+    ## SEPARATE CLOUD DISTANCE COLUMNS ##
+    cloudCols = [col for col in noCofactorDf.columns if col.startswith('cloud_dist_atom_')]
+    ## SELECT ATOMS IN PROTEIN DATAFRAME WITHIN CLOUDVALUE OF ANY CLOUD ATOM ##
+    withincloudRange = noCofactorDf[cloudCols].lt(cloudValue).any(axis=1)
+    cloudDf=noCofactorDf[withincloudRange]
+
+    return cloudDf
+        
+#######################################################################################
+def gen_protein_region(pdbDf,cofactorName):
+    ## REMOVE COFACTOR FROM PDB DATAFRAME ##
+    proteinDf = pdbDf[pdbDf["RES_NAME"] != cofactorName]
+    return proteinDf
+
 ########################################################################################
-def orb_count(orbCenter,pdbDf,aminoAcidNames,orbValue,proteinName,cofactorName):
-    # initialise amino acid count dataframe (better as a dict?)
+def element_count_in_region(regionDf,regionName,proteinName):
+    ## INITIALISE ELEMENT COUNT DATAFRAME ##
     columnNames=[]
-    columnNames.append(f'orb.total')
-    for aminoAcid in aminoAcidNames:
-        columnNames.append(f'orb.{aminoAcid}')
-    for element in ["C","N","O"]:
-        columnNames.append(f'orb.{element}')
-    columnNames=sorted(columnNames)
-    orbCountDf = pd.DataFrame(columns=columnNames,index=[proteinName])
-    # remove cofactor from pdbDf
-    pdbDf = pdbDf[pdbDf["RES_NAME"] != cofactorName].copy()
-    # get count of each AA within orbValue of orbycenter
-    # get distacnes to orbycenter
-    pdbDf.loc[:, "orb_DISTANCES"] = pdbDf.apply(calculateEuclideanDistance, point=orbCenter, axis=1)
-    # filter main dataframe by atoms within orbValue range of orbycenter
-    orbDf = pdbDf[pdbDf["orb_DISTANCES"] <= orbValue].copy()
-    if orbDf.empty:
-        orbCountDf.iloc[0] = 0
-        return orbCountDf
-    # count elements [Carbon, Nitrogen, Oxygen]
-    for element in ["C","N","O"]:
+    for element in ["C","N","O","S"]:
+        columnNames.append(f"{regionName}.{element}")
+    elementCountDf = pd.DataFrame(columns=columnNames,index=[proteinName])
+    ## COUNT ELEMENTS IN REGION, RETURN ZERO IF REGION HAS NONE OR DOES NOT EXIST
+    for element in ["C","N","O","S"]:
         try:
-            orbCountDf.loc[:,f'orb.{element}'] = orbDf["ELEMENT"].value_counts()[element]
+            elementCountDf.loc[:,f'{regionName}.{element}'] = regionDf["ELEMENT"].value_counts()[element]
         except:
-            orbCountDf.loc[:, f'orb.{element}'] = 0
- 
-    #  get unique residues to avoid over-counting
-    orbDf_uniqueRes=orbDf.drop_duplicates(subset=["RES_SEQ"])
-    # add individial and total amino acid counts to count featues dataframe
-    orbTotal=0
+            elementCountDf.loc[:, f'{regionName}.{element}'] = 0
+
+    return elementCountDf
+########################################################################################
+def amino_acid_count_in_region(regionDf, regionName, proteinName, aminoAcidNames):
+    ## INITIALSE AMINO ACID COUNT DATAFRAME ##
+    columnNames=[]
+    for aminoAcid in aminoAcidNames:
+        columnNames.append(f"{regionName}.{aminoAcid}")
+    aaCountDf = pd.DataFrame(columns=columnNames,index=[proteinName])
+
+    ## GET UNIQUE RESIDUES ONLY ##
+    uniqueResiduesDf = regionDf.drop_duplicates(subset = ["RES_SEQ"])
+
+    ## COUNT AMINO ACIDS IN REGION, RETURN ZERO IF REGION HAS NONE OR DOES NOT EXIST
+    totalResidueCount = []
     for aminoAcid in aminoAcidNames:
         try: 
-            orbCountDf.loc[:,f'orb.{aminoAcid}'] = orbDf_uniqueRes["RES_NAME"].value_counts()[aminoAcid]
+            aaCountDf.loc[:,f'{regionName}.{aminoAcid}'] = uniqueResiduesDf["RES_NAME"].value_counts()[aminoAcid]
         except:
-            orbCountDf.loc[:,f'orb.{aminoAcid}'] = 0
-        orbTotal+=orbCountDf.loc[:,f'orb.{aminoAcid}']
-    orbCountDf.loc[:,"orb.total"] = orbTotal
-    return orbCountDf
+            aaCountDf.loc[:,f'{regionName}.{aminoAcid}'] = 0
+        totalResidueCount+=aaCountDf.loc[:,f'{regionName}.{aminoAcid}']
+    aaCountDf.loc[:,f"{regionName}.total"] = totalResidueCount
+    return aaCountDf
 ########################################################################################
-def cloud_count(cloudCoordsDf,pdbDf,aminoAcidNames,cloudValue,proteinName):
-    # initialise amino acid count dataframe (better as a dict?)
-    columnNames=[]
-    columnNames.append(f'cloud.total')
-    for aminoAcid in aminoAcidNames:
-        columnNames.append(f'cloud.{aminoAcid}')
-    for element in ["C","N","O"]:
-        columnNames.append(f'cloud.{element}')
-    columnNames=sorted(columnNames)
-    cloudCountDf = pd.DataFrame(columns=columnNames,index=[proteinName])
-    # get count of each AA within cloudValue of any atom in cloud
-    # for each atom in cloud...
-    for i, (x,y,z) in cloudCoordsDf.iterrows():
-        # calculate distance to atom and write to its own column
-        columnName=f'cloud_dist_atom_{i}'
-        pdbDf.loc[:,columnName]=pdbDf.apply(calculateEuclideanDistance, point=[x,y,z], axis =1)
-    # identify cloud columns in pdbDf
-    cloudCols = [col for col in pdbDf.columns if col.startswith('cloud_dist_atom_')]
-    # filter main dataframe by atoms within cloudValue range of cloud atoms
-    withincloudRange = pdbDf[cloudCols].lt(cloudValue).any(axis=1)
-    cloudDf=pdbDf[withincloudRange]
-    # if no atoms are within range, return empty features dataframe
-    if cloudDf.empty:
-        cloudCountDf.iloc[0] = o
-        return cloudCountDf
-    # count elements [Carbon, Nitrogen, Oxygen]
-    for element in ["C","N","O"]:
-        try:
-            cloudCountDf.loc[:,f'cloud.{element}'] = cloudDf["ELEMENT"].value_counts()[element]
-        except KeyError as e:
-            cloudCountDf.loc[:, f'cloud.{element}'] = 0
-    # get unique residues only
-    cloudDf_uniqueRes= cloudDf.drop_duplicates(subset=["RES_SEQ"])
-    cloudTotal=0
-    # add individial and total amino acid counts to count featues dataframe
-    for aminoAcid in aminoAcidNames:
-        if aminoAcid in cloudDf["RES_NAME"].unique():
-            cloudCountDf.loc[:,f'cloud.{aminoAcid}'] = cloudDf_uniqueRes["RES_NAME"].value_counts()[aminoAcid]
-        else:
-            cloudCountDf.loc[:,f'cloud.{aminoAcid}'] = 0
-        cloudTotal+=cloudCountDf[f'cloud.{aminoAcid}']
-    cloudCountDf["cloud.total"] = cloudTotal
-    return cloudCountDf
-        
-########################################################################################
-def protein_count(pdbDf,aminoAcidNames,proteinName):
-    # initialise amino acid count dataframe (better as a dict?)
-    columnNames=[]
-    columnNames.append(f'protein.total')
-    for aminoAcid in aminoAcidNames:
-        columnNames.append(f'protein.{aminoAcid}')
-    for element in ["C","N","O"]:
-        columnNames.append(f'protein.{element}')
-    columnNames=sorted(columnNames)
-    proteinCountDf = pd.DataFrame(columns=columnNames,index=[proteinName])
-
-    # count elements [Carbon, Nitrogen,Oxygen]
-    for element in ["C","N","O"]:
-        try:
-            proteinCountDf.loc[:,f'protein.{element}'] = pdbDf["ELEMENT"].value_counts()[element]
-        except KeyError:
-            proteinCountDf.loc[:, f'protein.{element}'] = 0
-    # get count of each AA in the whole protein
-    # get unique residue IDs only
-    pdbDf_uniqueRes=pdbDf.drop_duplicates(subset=["RES_SEQ"])
-    proteinTotal=0
-    # add individial and total amino acid counts to count featues dataframe
-    for aminoAcid in aminoAcidNames:
-        if aminoAcid in pdbDf_uniqueRes["RES_NAME"].unique():
-            proteinCountDf.loc[:,f'protein.{aminoAcid}'] = pdbDf_uniqueRes["RES_NAME"].value_counts()[aminoAcid]
-        else:
-            proteinCountDf.loc[:,f'protein.{aminoAcid}'] = 0
-        proteinTotal+=proteinCountDf[f'protein.{aminoAcid}']
-    proteinCountDf["protein.total"] = proteinTotal
-    return proteinCountDf 
-
-########################################################################################
-def cloud_orb_protein_properties(orbCountDf,cloudCountDf,proteinCountDf,aminoAcidProperties,aminoAcidNames,proteinName):
-    # initialise pandas dataframe
-    columnNames=[]
+def calculate_amino_acid_properties_in_region(aaCountDf, aminoAcidNames, aminoAcidProperties, proteinName, regionName):
+    ## INITIALISE PROPERTIES DATAFRAME ##
+    columnNames = []
     for property in aminoAcidProperties.columns:
-        columnNames.append(f'orb.{property}')
-        columnNames.append(f'cloud.{property}')
-        columnNames.append(f'protein.{property}')
-    propertiesDf=pd.DataFrame(columns=columnNames,index=[proteinName])
+        columnNames.append(f"{regionName}.{property}")
+    propertiesDf = pd.DataFrame(columns=columnNames, index=[proteinName])
     
-    for region, countDf in zip(["orb","cloud","protein"],[orbCountDf,cloudCountDf,proteinCountDf]):
-        for property in aminoAcidProperties:
-            propertyValue=0
-            for aminoAcid in aminoAcidNames:
-                try:
-                    aaCount = countDf.at[proteinName,f"{region}.{aminoAcid}"]
-                except KeyError:
-                    aaCount = 0
-                aaPropertyvalue = aminoAcidProperties.at[aminoAcid,property]
-                value = aaCount * aaPropertyvalue
-                propertyValue += value 
+    ## LOOP THROUGH PROPERTIES SUPPLIED IN AMINO_ACID_TABLE.txt
+    for property in aminoAcidProperties:
+        propertyValue=0
+        for aminoAcid in aminoAcidNames:
             try:
-                totalAminoAcids=countDf.at[proteinName,f'{region}.total']
+                aaCount = aaCountDf.at[proteinName,f"{regionName}.{aminoAcid}"]
             except KeyError:
-                totalAminoAcids=0
-            if not totalAminoAcids == 0:
-                propertyValue = propertyValue / totalAminoAcids
-            propertiesDf[f'{region}.{property}'] = propertyValue
+                aaCount = 0
+            aaPropertyvalue = aminoAcidProperties.at[aminoAcid,property]
+            value = aaCount * aaPropertyvalue
+            propertyValue += value 
+        try:
+            totalAminoAcids=aaCountDf.at[proteinName,f'{regionName}.total']
+        except KeyError:
+            totalAminoAcids=0
+        if not totalAminoAcids == 0:
+            propertyValue = propertyValue / totalAminoAcids
+        propertiesDf[f'{regionName}.{property}'] = propertyValue
+
     return propertiesDf
 ########################################################################################
-def get_key_atom_features(keyAtomCoords, pdbDf, aminoAcidNames, aminoAcidProperties,proteinName,cofactorName):
-    # initialse dataframe
-    columnNames=[]
+
+def nearest_n_residues_to_key_atom(keyAtomCoords, pdbDf, aminoAcidNames, aminoAcidProperties,
+                                   proteinName,cofactorName, nNearestList) :
+    ## INITAILSE KEY ATOMS DATAFRAME ##       
+    columnNames = []
     for keyAtomId, (x,y,z) in keyAtomCoords.iterrows():
         for property in aminoAcidProperties:
-            columnNames.append(f'1_{keyAtomId}.{property}')
-            columnNames.append(f'3_{keyAtomId}.{property}')
+            for nNearest in nNearestList:
+                columnNames.append(f'{str(nNearest)}_{keyAtomId}.{property}')
     keyAtomFeaturesDf = pd.DataFrame(columns=columnNames, index=[proteinName])
-    # remove cofactor from pdbDf
-    pdbDf = pdbDf[pdbDf["RES_NAME"] != cofactorName].copy()
-    # loop through key atom coords
+
+
+    ## REMOVE COFACTOR FROM PDB DATAFRAME ##
+    noCofactorDf = pdbDf[pdbDf["RES_NAME"] != cofactorName]
+    ## LOOP THROUGH KEY ATOMS ##
     for keyAtomId, (x,y,z) in keyAtomCoords.iterrows():
-        # calculate distance for each atom in pdb dataframe
+        ## CALCULATE DISTANCE TO KEY ATOM FOR ALL PROTEIN ATOMS ##
         columnName=f'{keyAtomId}_distance'
-        pdbDf.loc[:,columnName]=pdbDf.apply(calculateEuclideanDistance, point=[x,y,z], axis =1)
-        # Get the unique residue sequences
+        noCofactorDf[columnName] = np.linalg.norm(noCofactorDf[["x", "y", "z"]].values - np.array(x,y,z), axis=1)
+        ## GET LIST OF UNIQUE RESIDUE NUMBERS ##
         uniqueResidues = pdbDf['RES_SEQ'].unique()
-        # Iterate over each unique residue and find the nearest point
+        ## FIND NEAREST ATOM IN EACH RESIDUE TO KEY ATOM ##
         nearestPointIndecies=[]
         for residueSeq in uniqueResidues:
-            residueData = pdbDf[pdbDf['RES_SEQ'] == residueSeq]
+            residueData = noCofactorDf[noCofactorDf['RES_SEQ'] == residueSeq]
             nearestPointIdx = residueData[columnName].idxmin()
-            nearestPointIndecies.append(nearestPointIdx)
-        # make a subset dataframe   
+            nearestPointIndecies.append(nearestPointIdx)    
+
+        ## GET NEW DATAFRAME CONTAINING ONLY NEAREST ATOMS OF EACH RESIDUE ##
         nearestPointsResidues = pdbDf.loc[nearestPointIndecies]
-        # Reset the index of the resulting DataFrame
+        ## RESET INDEX ##
         nearestPointsResidues.reset_index(drop=True, inplace=True)
-        # sort by distance to key atom
+        ## SORT BY DISTANCE TO KEY ATOM ##
         nearestResiduesSorted = nearestPointsResidues.sort_values(by=columnName, ascending=True)
-        # get nearest one and three 
-        nearestOneResidue=''.join(nearestResiduesSorted.head(1)["RES_NAME"].to_list())
-        nearestThreeResidues=nearestResiduesSorted.head(3)["RES_NAME"].to_list()
-        # update features dataframe for single nearest amino acid
-        for property in aminoAcidProperties:
-            propertyValue = aminoAcidProperties.at[nearestOneResidue,property]
-            keyAtomFeaturesDf.loc[proteinName,f'1_{keyAtomId}.{property}'] = propertyValue
-        # update features dataframe for three nearest amino acids
-        for property in aminoAcidProperties:
-            propertyValue=0
-            for aminoAcid in nearestThreeResidues:
-                value = aminoAcidProperties.at[aminoAcid,property]
-                propertyValue += value 
-            propertyValue = propertyValue / 3
-            keyAtomFeaturesDf.loc[proteinName,f'3_{keyAtomId}.{property}'] = propertyValue
-    return keyAtomFeaturesDf
+        ## LOOP THROUGH NNEARESTLIST ##
+        for nNearest in nNearestList:
+            ## GET LIST OF N NEAREST RESIDUES TO KEY ATOM ##
+            nearestResidueList = nearestResiduesSorted.head(1)["RES_NAME"].to_list()
+            ## GET PROPERTY VALUES ##
+            for property in aminoAcidProperties:
+                propertyValue=0
+                for aminoAcid in nearestResidueList:
+                    value = aminoAcidProperties.at[aminoAcid,property]
+                    propertyValue += value 
+                propertyValue = propertyValue / nNearest
+                ## ADD TO DATAFRAME ##
+                keyAtomFeaturesDf.loc[proteinName,f'{str(nNearest)}_{keyAtomId}.{property}'] = propertyValue
+        return keyAtomFeaturesDf
+########################################################################################
