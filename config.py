5aed2513ca818e5c7cf48e1ae8e763b9ea21241b
ESPhoenix <e.s.phoenix@hotmail.co.uk>
2023-11-08 12:58:12 +0000

    update README.md and config file

--- config.py
+++ config.py
@@ -1,24 +1,3 @@
-# coFeatures
-Cofactor-dependent feature generation for protiens
- 
- ## Clone Repository
- ```bash
- git clone https://github.com/ESPhoenix/coFeatures
- ```
-
- ## Create and Activate Conda Environment
- ```bash
- conda create -n coFeaturesEnv python=3.7.12
- conda activate coFeaturesEnv
- ```
-
- ## Install Required Python Packages
- ```bash
-pip install argpass==0.0.2 numpy==1.21.6 pandas==1.3.5 tqdm==4.66.1
- ```
-
- ## Edit Config File or Create Your Own
- ```python
 import numpy as np        
 def inputs():
         ## LOCATION OF YOUR PDB FILES
@@ -57,12 +36,3 @@ def inputs():
 
         return (inputDir, outDir, aminoAcidTable, cofactorNames, keyAtomsDict,
                     orbAtomsDict, cloudAtomsDict, orbRange, cloudRange)
-
- ```
-
- ## Useage
- From the coFeatures directory, run the following in the command line:
- ```bash
- python coFeatures.py --config config.py
- ```
-You may want to change the name of config.py to your own config file
